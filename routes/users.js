e08014c22912de934179e5dbf4fecb2b692e3c86
yana savchenko <y.s.10.06.13@gmail.com>
2018-11-15 10:34:01 +0300

    add update user data

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer({ dest: './public/files/' });
 const db = require('../models')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
 const checkAuth = require('../middlewares/authFunc');
 
 module.exports = (router) => {
@@ -17,9 +19,37 @@ module.exports = (router) => {
                     email: user.dataValues.email,
                     age: user.dataValues.age,
                 }
-                console.log(userData);
-                
+
                 return res.json(userData);
             })
+        })
+        .put(async (req, res) => {
+            try { 
+                const user = await db.user.findOne({
+                    where: {
+                        [Op.and]: [{ email: req.body.email },
+                        { id: { [Op.not]: req.user.id } }]
+                    }
+                })
+                
+                if (user) {
+                    return res.status(400).json({ message: 'Invalid email' })
+                }
+                await db.user.update({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    age: req.body.age
+                },
+                    { where: { id: req.user.id } })
+                    .then(() => {
+                        console.log('upd');
+                    });
+                return res.json("ok")
+
+            } catch (err) {
+                return res.status(500).json({ message: err.message })
+            }
+
         })
 }
