9acb0e7e7f328a1b432ca2c732c7f20617ba7a4b
yana savchenko <y.s.10.06.13@gmail.com>
2018-12-03 11:32:05 +0300

    Update home route

--- routes/recipes.js
+++ routes/recipes.js
@@ -126,18 +126,37 @@ module.exports = (router) => {
     })
 
   router.route('/home')
-    .get((req, res) => {
+    .get(async (req, res) => {
+      try{
       const limit = 6;
       const direction = 'DESC';
       const queryParams = {
         limit,
         order: [["createdAt", direction]]
       }
-      db.recipe.findAll(queryParams)
-        .then((recipes) => {
-          return res.json(recipes);
+      let recipes = await db.recipe.findAll(queryParams)
+      const token = req.get('Authorization');
+      if (token) {
+        const decoded = jwt.verify(token, config.secretJWT);
+        const user = await db.user.findById(decoded.id);
+        userFavor = await user.getRecipes();
+        userFavor = await _.groupBy(userFavor, el => el.dataValues.id);
+
+        recipes = recipes.map(recipe => {
+          const id = recipe.dataValues.id;
+          if (userFavor[id]) {
+            recipe.dataValues.isLiked = true;
+          }
+          return recipe;
         })
-        .catch(err => res.status(500).json({ message: err.message }))
+        return res.json(recipes);
+      }
+      return res.json(recipes);
+
+    }
+      catch (err) {
+        return res.status(500).json({ message: err.message });
+      }
     })
 
   router.route('/favorite')
@@ -147,7 +166,7 @@ module.exports = (router) => {
         let favor = await user.getRecipes();
         favor = favor.map(recipe => {
           recipe.dataValues.isLiked = true;
-          return recipe
+          return recipe;
         })
         return res.json(favor);
       } catch (err) {
